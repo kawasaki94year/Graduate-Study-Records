52e62d8f2d2273f6a575370cb2d2b50b90b207d6
Lang.Xu <104112142+kawasaki94year@users.noreply.github.com>
2023-04-10 18:50:57 +0800

    Update Divide-cd.c

--- root-tutorial/Divide-cd.c
+++ root-tutorial/Divide-cd.c
@@ -15,9 +15,13 @@ int main()
   //将画布分割成两行一列，第一个子区域为当前区域
   canvas->Divide(2,1);
   canvas->cd(1);
-  // 在第一个子区域中创建一个名为 "hist1" 的 TH1F 对象，并绘制它
+  // 在第一个子区域中创建一个名为 "hist1" 的 TH1F 对象，并设置x和y的范围以及名称
     TH1F* hist1 = new TH1F("hist1", "Histogram 1", 100, -5.0, 5.0);
     hist1->FillRandom("gaus", 1000);
+    hist1->GetXaxis()->SetTitle("X Axis Title");
+    hist1->GetYaxis()->SetTitle("Y Axis Title");
+    hist1->SetMinimum(0.0);
+    hist1->SetMaximum(200.0);
     hist1->Draw();
 
     // 将绘图操作切换到第二个子区域中
@@ -26,7 +30,23 @@ int main()
     // 在第二个子区域中创建一个名为 "hist2" 的 TH1F 对象，并绘制它
     TH1F* hist2 = new TH1F("hist2", "Histogram 2", 100, -5.0, 5.0);
     hist2->FillRandom("gaus", 1000);
+    hist2->GetXaxis()->SetTitle("X Axis Title");
+    hist2->GetYaxis()->SetTitle("Y Axis Title");
+    hist2->SetMinimum(0.0);
+    hist2->SetMaximum(150.0);
     hist2->Draw();
+
+    // 将绘图操作切换回第一个子区域中
+    canvas->cd(1);
+
+    // 画出第一个子区域的图形
+    hist1->Draw();
+
+    // 将绘图操作切换回整个画布
+    canvas->cd();
+
+    // 保存画布为 PNG 文件
+    canvas->SaveAs("mycanvas.png");
 
     // 等待用户关闭画布窗口
     canvas->Update();
