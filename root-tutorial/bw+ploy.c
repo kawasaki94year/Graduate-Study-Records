af72fc1add044f6e61505327211e6c6704a5bd68
Lang.Xu <104112142+kawasaki94year@users.noreply.github.com>
2023-04-20 11:03:28 +0800

    Update bw+ploy.c

--- root-tutorial/bw+ploy.c
+++ root-tutorial/bw+ploy.c
@@ -1,64 +1,58 @@
-#include "/workfs2/bes/xhy_20200622/workarea-665/bes3plotstyle/bes3plotstyle.c"
+#include "/home/xwl/root-file/bes3plotstyle.c"
 using namespace RooFit;
 void test40090()
 {
   SetStyle(); #设置RooPlot对象的属性，用于绘制拟合曲线或数据分布的图形表示
   SetPrelimStyle(); #用于设置测量结果图形的样式，SetPrelimStyle()函数将图形的填充样式设置为灰色的斜线填充，表示这是一个初步的测量结果，该函数通常在绘制测量结果的图形之前调用。
-  gSystem->Load("libRooFit"); #动态加载libRoofit库，因为RooRealVar需要使用它
-        gROOT->SetStyle("Pub");
+  gSystem->Load("libRooFit");#动态加载libRoofit库，因为RooRealVar需要使用它
+  gROOT->SetStyle("Pub");
 
   TCanvas *c1 = new TCanvas("c1", "c1", 0, 0, 800, 600);
-  TFile *file = new TFile("/besfs5/groups/tauqcd/xhy_20200622/AnaData/f1pipi_Databp_4.0090_Final.root ");
-  TTree *t = (TTree*) file->Get("DataReducedmin");
-   RooRealVar x("minf1_final", "m(#f1)", 1.1019,1.4619);
-   RooDataSet after("after", "after", t, x);
-
-   RooRealVar mean1("mean1", "mean1",1.2819,1.1019, 1.4619);//中心值，区间
-   RooRealVar sigma1("sigma1", "sigma1",0.0231701,0.00,0.04);//宽度
-   RooBreitWigner BW("BW"," BW fit ",x, mean1,sigma1);
-
-//   RooRealVar mean2("mean2", "mean2",-0.00001,0.00001 );
-//   RooRealVar sigma2("sigma2", "sigma2" ,0.0009,0., 0.001);
-//RooRealVar sigma2("sigma2", "sigma2", 0., 0.01);   
-//   RooGaussian gaus2("gaus2"," gaus fit ",x, mean2,sigma2);
-//   x.setBins(10000, "cache");   
-//   RooFFTConvPdf BG("BG", "breit-wigner (X) gauss ", x, BW, gaus2);
-
-
-//  BG.setBufferFraction(0.2);
-//  BG.setBufferStrategy(2);
-
+  TFile *file = new TFile("/home/xwl/root-file/KKeta_Data_all.root");
+  TTree *t = (TTree*) file->Get("Data");
+  RooRealVar pipE("pipE", "e(#pip)", 0.1,1.4);
+          //定义数据集
+  RooDataSet SUMD("SUMD", "SUMD", t, pipE);
+          //定义布里渊拟合函数和拟合函数的参数
+  RooRealVar mean1("mean1", "mean1",0.1,0.5,1);//中心值，区间
+  RooRealVar sigma1("sigma1", "sigma1",0.005,0.5,1);//宽度
+  RooBreitWigner BW("BW"," BW fit ",pipE, mean1,sigma1);
+          //定义一阶切比雪夫合函数和拟合函数的参数(常用于背景拟合,定义的区间在[-1,1])
   RooRealVar p1("p1", "p1",0.172560,-1,1);
-  RooRealVar p2("p2", "p2",-0.100278,-0.5,0.5);
-  RooChebychev poly("poly", "poly PDF", x, RooArgList(p1,p2));
-
+  RooRealVar p2("p2", "p2",0.1,-1,1);
+  RooChebychev poly("poly", "poly PDF", pipE, RooArgList(p1,p2));
+          //定义信号和背景的权重 
    RooRealVar n_sig("n_sig", "signal number",100,0,10000);
    RooRealVar n_bkg("n_bkg", "bkg  number",1000,0,100000);
+          //创建信号和背景的加权和分布
    RooAddPdf sum2("sum2","sum2",RooArgList(BW,poly),RooArgList(n_sig, n_bkg));
-   sum2.fitTo( after , Range(1.1019,1.4619),Extended(kTRUE));
-   RooPlot* xframe = x.frame();
-   after.plotOn(xframe, Binning(80),DataError(RooAbsData::SumW2));
-   sum2.plotOn(xframe, LineColor(1));
+          //对数据进行拟合
+   sum2.fitTo(SUMD, Range(0.1,1.4),Extended(kTRUE));
+           //绘制pipE的分布和拟合结果
+   RooPlot* pipEframe = pipE.frame();
+   SUMD.plotOn(pipEframe, Binning(80),DataError(RooAbsData::SumW2));
+   sum2.plotOn(pipEframe, LineColor(1));
 
-   double chi2 = xframe->chiSquare();
-   sum2.plotOn(xframe, Components("BW"), LineStyle(kDashed), LineColor(2));
-   sum2.plotOn(xframe, Components("poly"),LineWidth(2), LineColor(4));
-//   BG.plotOn(xframe, Components("BW"),LineWidth(2), LineColor(5));
-   //sum2.plotOn(xframe, Components("BW"),LineWidth(2), LineColor(3));  
+   //double chi2 = xframe->chiSquare(); //计算拟合的chi2
+               //将拟合结果中贡献为“BW”的部分以虚线形式绘制在“xframe”上
+   sum2.plotOn(pipEframe, Components("BW"), LineStyle(kDashed), LineColor(2));
+               //将拟合结果中贡献为“poly”的部分以实线形式绘制在“xframe”上
+   sum2.plotOn(pipEframe, Components("poly"),LineWidth(2), LineColor(4));
 
    c1->SetFillColor(10);
    c1->cd();
-   xframe->SetXTitle("M(#pi#pi#eta)(GeV/c^{2})");
-   xframe->SetYTitle("Events");
-   FormatAxis(xframe->GetXaxis());
-   FormatAxis(xframe->GetYaxis());
+   pipEframe->SetXTitle("E(#pi^{+})(GeV/c^{2})");
+   pipEframe->SetYTitle("Events");
+        //对pipEframe对象的x轴和y轴进行格式化处理
+   FormatAxis(pipEframe->GetXaxis());
+   FormatAxis(pipEframe->GetYaxis());
 
-   xframe->Draw();
-cout<<"chi2="<<chi2<<endl;
+   pipEframe->Draw();
+//cout<<"chi2="<<chi2<<endl;
 
 ////////////////////////
 
-   TPaveText *pt = new TPaveText(0.6,0.8, 0.94,0.94,"BRNDC");
+   /*TPaveText *pt = new TPaveText(0.6,0.8, 0.94,0.94,"BRNDC");
    pt->SetBorderSize(1);
    pt->SetFillColor(10);
    pt->SetTextAlign(12);
@@ -75,10 +69,11 @@ cout<<"chi2="<<chi2<<endl;
    TString Par3V = Form("%5.5f", sigma1.getVal() );
    TString Par3E = Form("%3.5f", sigma1.getError() );
    TString Par3  = "bwsigma = " + Par3V + " #pm " + Par3E;
-  TString Par4V = Form("%5.5f", mean1.getVal() );
+   
+   TString Par4V = Form("%5.5f", mean1.getVal() );
    TString Par4E = Form("%3.5f", mean1.getError() );
    TString Par4  = "bwmean = " + Par4V + " #pm " + Par4E;
-
+   
    TString Par5V = Form("%5.5f", chi2 );
 //   TString Par4E = Form("%3.5f", mean1.getError() );
    TString Par5  = "chi2 = " + Par5V;
@@ -90,11 +85,14 @@ cout<<"chi2="<<chi2<<endl;
    text = pt->AddText(Par3);
    text = pt->AddText(Par4);
    text = pt->AddText(Par5);
-   pt->Draw();
+   pt->Draw();*/
 
-/*
-c1->Print("data_f1_21250.pdf");
-c1->Print("data_f1_21250.eps");
+
+c1->Print("test.pdf");
+c1->Print("test.eps");
 
  cout << "####job done!####" << endl;
-*/
+
+
+
+}
